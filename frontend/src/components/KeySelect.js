97d0f8814f374ea0425c543de6a6a1af2d49bed2
mnbcp <bc.peters@outlook.com>
2025-02-08 16:59:10 -0600

    Coded KeySelect.js

---
+++ frontend/src/components/KeySelect.js
@@ -0,0 +1,28 @@
+// KeySelect.js
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const KeySelect = ({ user, setView, setSelectedKey }) => {
+    const [availableKeys, setAvailableKeys] = useState([]);
+
+    useEffect(() => {
+        const fetchAvailableKeys = async () => {
+            const response = await axios.post('/available-keys', { pin: user.pin });
+            setAvailableKeys(response.data.availableKeys);            
+        };
+
+        fetchAvailableKeys();
+    }, [user.pin]);
+
+    return (
+        <div>
+          <h2>Select a Key</h2>
+          {availableKeys.map((key, index) => (
+            <div key={index} onClick={() => { setSelectedKey(key); setView('ConfirmPage'); }}>
+              {key}
+            </div>
+          ))}
+        </div>
+    );
+};
