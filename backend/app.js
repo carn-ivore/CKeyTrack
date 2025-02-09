317700232e2d53a525335cf4832a26c224e3c4c6
mnbcp <bc.peters@outlook.com>
2025-02-08 21:13:39 -0600

    added something about CORS on lines 4 and 8

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 // app.js
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 // Middleware to parse JSON requests
 app.use(express.json());
 
