221333efdcba9fd5772be0517f80c1776ca4500a
mnbcp <bc.peters@outlook.com>
2025-02-09 11:01:01 -0600

    Removed the ConfirmPage import line

--- frontend/src/components/KeySelect.js
+++ frontend/src/components/KeySelect.js
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './KeySelect.css'; // Import the CSS file
-import ConfirmPage from './ConfirmPage'; // Import ConfirmPage
 
 const KeySelect = ({ user, setView, setSelectedKey }) => {
     const [availableKeys, setAvailableKeys] = useState([]);
