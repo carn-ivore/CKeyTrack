a7f2367e0745364d5280d2e202b0c7f2992d2c39
mnbcp <bc.peters@outlook.com>
2025-02-10 12:11:42 -0600

    added a / to the const authroutes import

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // Import routes
-const authRoutes = require('.routes/authRoutes');
+const authRoutes = require('./routes/authRoutes');
 const availableKeysRoute = require('./routes/availableKeys');
 
 // Use routes
