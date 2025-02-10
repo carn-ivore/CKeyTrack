fcd44d50501f20c8cd2eace62b0a183ffea00ea8
mnbcp <bc.peters@outlook.com>
2025-02-10 13:35:48 -0600

    Maybe all the import thigns nood to be in line

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,8 +1,6 @@
 // LoginPage.js
 
-import React, { useState } from 'react';
-import React, { useState, useRef } from 'react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import './LoginPage.css'; // Import th CSS file
 
