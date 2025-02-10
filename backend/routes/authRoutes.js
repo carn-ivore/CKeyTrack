a6f6798f28635472503c827cf718cbbc284ae92a
mnbcp <bc.peters@outlook.com>
2025-02-10 12:20:55 -0600

    I properly identified the router variable at the beginning of the code.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const { google } = require('googleapis');
+const router = express.Router();
 const SERVICE_ACCOUNT_FILE = process.env.SERVICE_ACCOUNT_FILE;
 
 // Google Sheets document ID
