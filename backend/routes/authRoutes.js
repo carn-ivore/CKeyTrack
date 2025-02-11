65d12ea23eb7006b100d94d6e87f9fee318194ed
Brent <bc.peters@outlook.com>
2025-02-11 14:39:12 -0600

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,6 +11,7 @@ router.post('/', async (req, res) => {
     const { pin } = req.body;
 
   try {
+    // Create a Google Sheets API client
     const sheets = google.sheets({ version: 'v4', auth });
     // Read employee data from Google Sheets
     const response = await sheets.spreadsheets.values.get({
